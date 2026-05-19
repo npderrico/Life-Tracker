0f3e03b2918eaec2efeab4b48d10fbcdcd9feaac
Nick Derrico <nickderrico@Mac.lan>
2026-05-19 17:15:28 -0400

    Better error handling in firebase-config function

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -1,7 +1,5 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-  body: JSON.stringify({
+exports.handler = async () => {
+  const config = {
     apiKey:            process.env.FB_API_KEY,
     authDomain:        process.env.FB_AUTH_DOMAIN,
     databaseURL:       process.env.FB_DATABASE_URL,
@@ -9,5 +7,20 @@ exports.handler = async () => ({
     storageBucket:     process.env.FB_STORAGE_BUCKET,
     messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
     appId:             process.env.FB_APP_ID,
-  })
-});
+  };
+
+  const missing = Object.entries(config).filter(([,v]) => !v).map(([k]) => k);
+  if (missing.length) {
+    return {
+      statusCode: 503,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ error: 'Missing env vars: ' + missing.join(', ') })
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(config)
+  };
+};
